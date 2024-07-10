e218981d61171e7e1e8f3193b60182470b8e736e
Polina Berest <pamapolya@gmail.com>
2024-07-10 23:11:53 +0300

    Finished the home page

--- src/app/components/public/home/home.component.ts
+++ src/app/components/public/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private elementRef: ElementRef) {}
 
+  ngOnInit() {
+    const fragment = window.location.hash;
+    if (fragment) {
+      this.scrollToElement(fragment.replace('#', ''));
+    }
+  }
+
+  scrollToElement(id: string) {
+    const element = this.elementRef.nativeElement.querySelector(`#${id}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
